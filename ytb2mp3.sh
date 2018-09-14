7700786b4835adf945ffd251509172adbe987796
StephGit <mail@stephangirod.ch>
2018-09-14 17:35:04 +0200

    added path var to execute script

--- ytb2mp3.sh
+++ ytb2mp3.sh
@@ -1,14 +1,24 @@
- #!/bin/bash                                                                                    
- URL=$1                                                                                                                                                                                      
-                                                                                                
- # download audio and convert to mp3                                                  
- download_and_convert() {                                                                       
-  youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "%(title)s.%(ext)s" $URL                                                                              
- }                                                                                              
-                                                                                                
- # check parameter                                                                              
- if [ $# = 0 ]; then                                                                            
-  echo "Youtube URL is required."                                                                                       
- else                                                                                           
-  download_and_convert                                                                         
- fi          
+#!/bin/bash
+URL=$1
+DIR=$2
+
+# download audio and convert to mp3
+download_and_convert() {
+  youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --output "$DIR/%(title)s.%(ext)s" $URL
+}
+
+check_path() {
+  if [ -z "$DIR" ]; then
+    DIR="~/Music"
+  else
+    mkdir -p "$DIR"
+  fi
+}
+
+# check param
+if [ $# = 0 ]; then
+ echo "Download-URL is required"
+else
+ check_path
+ download_and_convert
+fi
